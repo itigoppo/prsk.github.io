93e9571632a9bae925d61d71c68450b9fdbc1973
itigoppo <itigoppo+github@gmail.com>
2021-12-27 22:00:18 +0900

    共通化しかけてたところなの忘れてたから一旦戻しとく

--- character_sort/js/sort.js
+++ character_sort/js/sort.js
@@ -45,6 +45,25 @@ $(function () {
   });
 });
 
+const loadUnitData = () => {
+  const units = {};
+  $.ajaxSetup({async: false}); // 同期通信(json取ってくるまで待つ)
+  $.getJSON("../data/units.json", function (data) {
+    $(data.units).each(function () {
+      units[this.id] = {
+        id: this.id,
+        name: this.unit,
+        short: this.short,
+        color: this.color,
+        sp_bg: this.sp_bg,
+      };
+    });
+  });
+  $.ajaxSetup({async: true}); // 非同期に戻す
+
+  return units;
+}
+
 const loadMembers = () => {
   const units = loadUnitData();
 
